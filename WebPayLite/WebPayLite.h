797f70875c37adfac111003ed0d61845441360b1
kyoro <kyoro@hakamastyle.net>
2013-12-01 15:25:25 -0800

    init class files

---
+++ WebPayLite/WebPayLite.h
@@ -0,0 +1,13 @@
+//
+//  WebPayLite.h
+//  WebPayLite
+//
+//  Created by Kyosuke INOUE on 2013/12/01.
+//  Copyright (c) 2013年 OpenSwipe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WebPayLite : NSObject
+
+@end
---
+++ WebPayLite/WebPayLite.m
@@ -0,0 +1,13 @@
+//
+//  WebPayLite.m
+//  WebPayLite
+//
+//  Created by Kyosuke INOUE on 2013/12/01.
+//  Copyright (c) 2013 OpenSwipe. All rights reserved.
+//
+
+#import "WebPayLite.h"
+
+@implementation WebPayLite
+
+@end
